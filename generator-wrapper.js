1c82c0b50b2d1229b8ffab700838a7dd1b0e62e2
Gadabout <gadabout.code@gmail.com>
2018-02-28 13:37:22 -0800

    get next in recursive crawler from closure

--- generator-wrapper.js
+++ generator-wrapper.js
@@ -1,25 +1,24 @@
 module.exports = generatorWrapper
 
 function generatorWrapper(fn) {
-  let recursive = (gen, lastValue) => {
-    let { value, done } = gen.next(lastValue)
-    if (done && value === undefined) { 
-      return
-    } else if (value instanceof Promise) {
-      value
-        .then(v => recursive(gen, v))
-        .catch(e => console.log('error in generatorWrapper promise', e))
-    } else {
-        recursive(gen, value)
+  return (req, res, next) => {
+    let recursive = (gen, lastValue) => {
+      let { value, done } = gen.next(lastValue)
+      if (done && value === undefined) { 
+        return
+      } else if (value instanceof Promise) {
+        value
+          .then(v => recursive(gen, v))
+          .catch(next)
+      } else {
+          recursive(gen, value)
+      }
     }
-  }
-
-  return (req, res) => {
+        
     try {
       recursive(fn(req, res))
     } catch(e) {
-      console.log('error cought in generatorWrapper', e)
+      console.log('error caught in generatorWrapper', e)
     }
-
   }
 }
