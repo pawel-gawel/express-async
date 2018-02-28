202dac7d10d8e7ec66e461eaab78484d30aca505
Gadabout <gadabout.code@gmail.com>
2018-02-28 14:50:32 -0800

    code tidying up

--- generator-wrapper.js
+++ generator-wrapper.js
@@ -1,24 +1,16 @@
-module.exports = generatorWrapper
-
-function generatorWrapper(fn) {
-  return (req, res, next) => {
-    let recursive = (gen, lastValue) => {
-      let { value, done } = gen.next(lastValue)
-      if (done && value === undefined) { 
-        return
-      } else if (value instanceof Promise) {
-        value
-          .then(v => recursive(gen, v))
-          .catch(next)
-      } else {
-          recursive(gen, value)
-      }
+module.exports = fn => (req, res, next) => {
+  let recursive = (gen, lastValue) => {
+    let { value, done } = gen.next(lastValue)
+    if (value instanceof Promise) {
+      value.then(v => recursive(gen, v)).catch(next)
+    } else if (!done) {
+        recursive(gen, value)
     }
-        
-    try {
-      recursive(fn(req, res))
-    } catch(e) {
-      console.log('error caught in generatorWrapper', e)
-    }
+  }
+      
+  try {
+    recursive(fn(req, res))
+  } catch (e) {
+    console.log('error caught in generatorWrapper', e)
   }
 }
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = require('express')()
 app.use(asyncRoutes)
 app.use(generatorRoutes)
 
+app.use((err, req, res, next) => {
+  res.send("There was an error: " + err.message)
+})
+
 app.listen(8000, () => {
   console.log('it listens')
   
